65aabc8798a802eab159dbc036281e4d914a9f6a
Muhajudin <muhajudin@gmail.com>
2015-10-09 10:14:02 +0700

    Tugas 2

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,202 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.textHasil = New System.Windows.Forms.TextBox()
+        Me.tombolTujuh = New System.Windows.Forms.Button()
+        Me.tombolEmpat = New System.Windows.Forms.Button()
+        Me.tombolSatu = New System.Windows.Forms.Button()
+        Me.tombolNol = New System.Windows.Forms.Button()
+        Me.tombolDelapan = New System.Windows.Forms.Button()
+        Me.tombolLima = New System.Windows.Forms.Button()
+        Me.tombolDua = New System.Windows.Forms.Button()
+        Me.tombolSembilan = New System.Windows.Forms.Button()
+        Me.tombolEnam = New System.Windows.Forms.Button()
+        Me.tombolTiga = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.tombolHapus = New System.Windows.Forms.Button()
+        Me.tombolKurang = New System.Windows.Forms.Button()
+        Me.tombolTambah = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'textHasil
+        '
+        Me.textHasil.Location = New System.Drawing.Point(11, 10)
+        Me.textHasil.Name = "textHasil"
+        Me.textHasil.ReadOnly = True
+        Me.textHasil.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.textHasil.Size = New System.Drawing.Size(265, 20)
+        Me.textHasil.TabIndex = 0
+        Me.textHasil.Text = "0"
+        '
+        'tombolTujuh
+        '
+        Me.tombolTujuh.Location = New System.Drawing.Point(12, 36)
+        Me.tombolTujuh.Name = "tombolTujuh"
+        Me.tombolTujuh.Size = New System.Drawing.Size(50, 49)
+        Me.tombolTujuh.TabIndex = 0
+        Me.tombolTujuh.Text = "7"
+        Me.tombolTujuh.UseVisualStyleBackColor = True
+        '
+        'tombolEmpat
+        '
+        Me.tombolEmpat.Location = New System.Drawing.Point(12, 91)
+        Me.tombolEmpat.Name = "tombolEmpat"
+        Me.tombolEmpat.Size = New System.Drawing.Size(50, 49)
+        Me.tombolEmpat.TabIndex = 1
+        Me.tombolEmpat.Text = "4"
+        Me.tombolEmpat.UseVisualStyleBackColor = True
+        '
+        'tombolSatu
+        '
+        Me.tombolSatu.Location = New System.Drawing.Point(12, 146)
+        Me.tombolSatu.Name = "tombolSatu"
+        Me.tombolSatu.Size = New System.Drawing.Size(50, 49)
+        Me.tombolSatu.TabIndex = 2
+        Me.tombolSatu.Text = "1"
+        Me.tombolSatu.UseVisualStyleBackColor = True
+        '
+        'tombolNol
+        '
+        Me.tombolNol.Location = New System.Drawing.Point(12, 201)
+        Me.tombolNol.Name = "tombolNol"
+        Me.tombolNol.Size = New System.Drawing.Size(50, 49)
+        Me.tombolNol.TabIndex = 3
+        Me.tombolNol.Text = "0"
+        Me.tombolNol.UseVisualStyleBackColor = True
+        '
+        'tombolDelapan
+        '
+        Me.tombolDelapan.Location = New System.Drawing.Point(84, 36)
+        Me.tombolDelapan.Name = "tombolDelapan"
+        Me.tombolDelapan.Size = New System.Drawing.Size(50, 49)
+        Me.tombolDelapan.TabIndex = 4
+        Me.tombolDelapan.Text = "8"
+        Me.tombolDelapan.UseVisualStyleBackColor = True
+        '
+        'tombolLima
+        '
+        Me.tombolLima.Location = New System.Drawing.Point(84, 91)
+        Me.tombolLima.Name = "tombolLima"
+        Me.tombolLima.Size = New System.Drawing.Size(50, 49)
+        Me.tombolLima.TabIndex = 5
+        Me.tombolLima.Text = "5"
+        Me.tombolLima.UseVisualStyleBackColor = True
+        '
+        'tombolDua
+        '
+        Me.tombolDua.Location = New System.Drawing.Point(84, 146)
+        Me.tombolDua.Name = "tombolDua"
+        Me.tombolDua.Size = New System.Drawing.Size(50, 49)
+        Me.tombolDua.TabIndex = 6
+        Me.tombolDua.Text = "2"
+        Me.tombolDua.UseVisualStyleBackColor = True
+        '
+        'tombolSembilan
+        '
+        Me.tombolSembilan.Location = New System.Drawing.Point(155, 36)
+        Me.tombolSembilan.Name = "tombolSembilan"
+        Me.tombolSembilan.Size = New System.Drawing.Size(50, 49)
+        Me.tombolSembilan.TabIndex = 7
+        Me.tombolSembilan.Text = "9"
+        Me.tombolSembilan.UseVisualStyleBackColor = True
+        '
+        'tombolEnam
+        '
+        Me.tombolEnam.Location = New System.Drawing.Point(155, 91)
+        Me.tombolEnam.Name = "tombolEnam"
+        Me.tombolEnam.Size = New System.Drawing.Size(50, 49)
+        Me.tombolEnam.TabIndex = 8
+        Me.tombolEnam.Text = "6"
+        Me.tombolEnam.UseVisualStyleBackColor = True
+        '
+        'tombolTiga
+        '
+        Me.tombolTiga.Location = New System.Drawing.Point(155, 146)
+        Me.tombolTiga.Name = "tombolTiga"
+        Me.tombolTiga.Size = New System.Drawing.Size(50, 49)
+        Me.tombolTiga.TabIndex = 9
+        Me.tombolTiga.Text = "3"
+        Me.tombolTiga.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(81, 201)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(124, 48)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "="
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'tombolHapus
+        '
+        Me.tombolHapus.Location = New System.Drawing.Point(220, 36)
+        Me.tombolHapus.Name = "tombolHapus"
+        Me.tombolHapus.Size = New System.Drawing.Size(52, 104)
+        Me.tombolHapus.TabIndex = 11
+        Me.tombolHapus.Text = "clear"
+        Me.tombolHapus.UseVisualStyleBackColor = True
+        '
+        'tombolKurang
+        '
+        Me.tombolKurang.Location = New System.Drawing.Point(222, 146)
+        Me.tombolKurang.Name = "tombolKurang"
+        Me.tombolKurang.Size = New System.Drawing.Size(50, 49)
+        Me.tombolKurang.TabIndex = 12
+        Me.tombolKurang.Text = "-"
+        Me.tombolKurang.UseVisualStyleBackColor = True
+        '
+        'tombolTambah
+        '
+        Me.tombolTambah.Location = New System.Drawing.Point(222, 200)
+        Me.tombolTambah.Name = "tombolTambah"
+        Me.tombolTambah.Size = New System.Drawing.Size(50, 49)
+        Me.tombolTambah.TabIndex = 13
+        Me.tombolTambah.Text = "+"
+        Me.tombolTambah.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.tombolTambah)
+        Me.Controls.Add(Me.tombolKurang)
+        Me.Controls.Add(Me.tombolHapus)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.tombolTiga)
+        Me.Controls.Add(Me.tombolEnam)
+        Me.Controls.Add(Me.tombolSembilan)
+        Me.Controls.Add(Me.tombolDua)
+        Me.Controls.Add(Me.tombolLima)
+        Me.Controls.Add(Me.tombolDelapan)
+        Me.Controls.Add(Me.tombolNol)
+        Me.Controls.Add(Me.tombolSatu)
+        Me.Controls.Add(Me.tombolEmpat)
+        Me.Controls.Add(Me.tombolTujuh)
+        Me.Controls.Add(Me.textHasil)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = " "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents textHasil As System.Windows.Forms.TextBox
+    Friend WithEvents tombolTujuh As System.Windows.Forms.Button
+    Friend WithEvents tombolEmpat As System.Windows.Forms.Button
+    Friend WithEvents tombolSatu As System.Windows.Forms.Button
+    Friend WithEvents tombolNol As System.Windows.Forms.Button
+    Friend WithEvents tombolDelapan As System.Windows.Forms.Button
+    Friend WithEvents tombolLima As System.Windows.Forms.Button
+    Friend WithEvents tombolDua As System.Windows.Forms.Button
+    Friend WithEvents tombolSembilan As System.Windows.Forms.Button
+    Friend WithEvents tombolEnam As System.Windows.Forms.Button
+    Friend WithEvents tombolTiga As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents tombolHapus As System.Windows.Forms.Button
+    Friend WithEvents tombolKurang As System.Windows.Forms.Button
+    Friend WithEvents tombolTambah As System.Windows.Forms.Button
 
 End Class
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,106 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
+    Dim Masukan1 As Integer
+    Dim Masukan2 As Integer
+    Dim operasi As String
+    Dim status As Boolean
 
+    Private Sub inputNomor(ByVal masukan As Char)
+        If Me.status = True Then
+            If (Me.textHasil.Text = "0") Then
+                Me.textHasil.Text = Convert.ToString(masukan)
+            Else
+                Dim textHasil As TextBox = Me.textHasil
+                textHasil.Text = (textHasil.Text & Convert.ToString(masukan))
+            End If
+        Else
+            Me.textHasil.Text = Convert.ToString(masukan)
+            Me.status = True
+        End If
+    End Sub
+
+    Private Sub inputOperator(ByVal oprator As String)
+        If (Me.textHasil.Text <> "0") Then
+            If (Me.operasi = String.Empty) Then
+                Me.Masukan1 = Convert.ToInt32(Me.textHasil.Text)
+                Me.textHasil.Text = Me.Masukan1.ToString
+                Me.status = False
+            Else
+                Me.hitung()
+            End If
+            Me.operasi = oprator
+        End If
+    End Sub
+
+    Private Sub hitung()
+        Me.Masukan2 = Convert.ToInt32(Me.textHasil.Text)
+        Select Case Me.operasi
+            Case "+"
+                Me.Masukan1 = (Me.Masukan1 + Me.Masukan2)
+                Exit Select
+            Case "-"
+                Me.Masukan1 = (Me.Masukan1 - Me.Masukan2)
+                Exit Select
+        End Select
+        Me.textHasil.Text = Me.Masukan1.ToString
+        Me.status = False
+    End Sub
+    Private Sub tombolNol_Click(sender As Object, e As EventArgs) Handles tombolNol.Click
+        If (Me.status AndAlso (Me.textHasil.Text <> "0")) Then
+            Dim textHasil As TextBox = Me.textHasil
+            textHasil.Text = (textHasil.Text & "0")
+        End If
+    End Sub
+
+    Private Sub tombolSatu_Click(sender As Object, e As EventArgs) Handles tombolSatu.Click
+        Me.inputNomor("1")
+    End Sub
+
+    Private Sub tombolDua_Click(sender As Object, e As EventArgs) Handles tombolDua.Click
+        Me.inputNomor("2")
+    End Sub
+
+    Private Sub tombolTiga_Click(sender As Object, e As EventArgs) Handles tombolTiga.Click
+        Me.inputNomor("3")
+    End Sub
+
+    Private Sub tombolEmpat_Click(sender As Object, e As EventArgs) Handles tombolEmpat.Click
+        Me.inputNomor("4")
+    End Sub
+
+    Private Sub tombolLima_Click(sender As Object, e As EventArgs) Handles tombolLima.Click
+        Me.inputNomor("5")
+    End Sub
+
+    Private Sub tombolEnam_Click(sender As Object, e As EventArgs) Handles tombolEnam.Click
+        Me.inputNomor("6")
+    End Sub
+
+    Private Sub tombolTujuh_Click(sender As Object, e As EventArgs) Handles tombolTujuh.Click
+        Me.inputNomor("7")
+    End Sub
+
+    Private Sub tombolDelapan_Click(sender As Object, e As EventArgs) Handles tombolDelapan.Click
+        Me.inputNomor("8")
+    End Sub
+
+    Private Sub tombolSembilan_Click(sender As Object, e As EventArgs) Handles tombolSembilan.Click
+        Me.inputNomor("9")
+    End Sub
+    Private Sub tombolHapus_Click_1(sender As Object, e As EventArgs) Handles tombolHapus.Click
+        Me.textHasil.Text = 0
+        Me.Masukan1 = 0
+        Me.operasi = String.Empty
+    End Sub
+    Private Sub tombolKurang_Click_1(sender As Object, e As EventArgs) Handles tombolKurang.Click
+        Me.inputOperator("-")
+    End Sub
+    Private Sub tombolTambah_Click_1(sender As Object, e As EventArgs) Handles tombolTambah.Click
+        Me.inputOperator("+")
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Me.hitung()
+        Me.operasi = String.Empty
+    End Sub
 End Class
